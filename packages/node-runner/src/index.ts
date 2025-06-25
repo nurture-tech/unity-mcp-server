b139fd1c972dbc6fbe91279d7d428bd0bf422c0d
Alex Stone <stone@filamentgames.com>
2025-06-24 23:32:31 -0500

    wip runner script

--- packages/node-runner/src/index.ts
+++ packages/node-runner/src/index.ts
@@ -1,1 +1,31 @@
-﻿console.log("Hello world");
+﻿import { readFileSync } from "node:fs";
+import { execSync, exec } from "node:child_process";
+import { platform } from "node:process";
+import { ArgumentParser } from "argparse";
+
+const parser = new ArgumentParser();
+parser.add_argument("-projectPath", { type: String, required: true });
+const args = parser.parse_args();
+const projectPath = args.projectPath;
+const projectVersion = readFileSync(`${projectPath}/ProjectSettings/ProjectVersion.txt`, "utf8").match(/m_EditorVersion:\s*(.*)/)?.[1] || "";
+
+const editorPathCommand =
+  platform === "darwin"
+    ? `/Applications/Unity\\ Hub.app/Contents/MacOS/Unity\\ Hub -- --headless editors -i ${projectVersion}`
+    : `"${process.env.PROGRAMFILES}\\Unity Hub\\Unity Hub.exe" -- --headless editors -i  ${projectVersion}`;
+
+const editorPaths = String(execSync(editorPathCommand)).trim();
+
+const editorPath =
+  `${editorPaths}`
+    .split("\n")
+    .find((line) => line.includes(projectVersion))
+    ?.split(" , installed at ")[1] || "";
+
+const proc = exec(`${editorPath} ${process.argv.slice(1).join(" ")}`);
+
+await new Promise((resolve) => {
+  proc.on("exit", (code) => {
+    resolve(code);
+  });
+});
