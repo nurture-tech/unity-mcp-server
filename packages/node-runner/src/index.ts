cf2b2368786422e8eb240d2f509b7ac662adee33
Alex Stone <1000237+st0nerhat@users.noreply.github.com>
2025-06-25 01:19:30 -0500

    Update to MCP SDK v0.3.0-preview.1 (#2)

---
+++ packages/node-runner/src/index.ts
@@ -0,0 +1,31 @@
+﻿import { spawn } from "node:child_process";
+import { exit } from "node:process";
+import { ArgumentParser } from "argparse";
+
+const parser = new ArgumentParser();
+parser.add_argument("-unityPath", { type: String, required: true });
+parser.add_argument("", { nargs: "*" });
+const args = parser.parse_args();
+const unityPath = args.unityPath;
+
+const proc = spawn(unityPath, [...process.argv.slice(1), "-mcp", "-logFile", "-"]);
+
+const code = await new Promise<number | null>((resolve, reject) => {
+  process.stdin.pipe(proc.stdin!);
+  proc.stdout?.on("data", (data) => {
+    const lines = data.toString().split("\n");
+    for (const line of lines) {
+      if (line.startsWith("{")) {
+        process.stdout.write(line + "\n");
+      }
+    }
+  });
+  proc.on("exit", (code) => {
+    resolve(code);
+  });
+  proc.on("error", (err) => {
+    reject(err.message);
+  });
+});
+
+exit(code ?? 0);
---
+++ packages/unity/Editor/Logger.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using UnityEngine;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+using Object = UnityEngine.Object;
+
+namespace Nurture.MCP.Editor
+{
+    class UnityMcpLogger : ILogger
+    {
+        private readonly string _categoryName;
+        private readonly LogLevel[] _levels;
+
+        public UnityMcpLogger(string categoryName = null, LogLevel[] levels = null)
+        {
+            _categoryName = categoryName;
+            _levels = levels;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+            where TState : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return _levels == null || _levels.Contains(logLevel);
+        }
+
+        public void Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception exception,
+            Func<TState, Exception, string> formatter
+        )
+        {
+            Debug.Log($"[MCP] {logLevel}: {formatter(state, exception)}");
+        }
+    }
+
+    class UnityLoggerFactory : ILoggerFactory
+    {
+        private LogLevel[] _levels;
+
+        public UnityLoggerFactory(LogLevel[] levels)
+        {
+            _levels = levels;
+        }
+
+        public void Dispose() { }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new UnityMcpLogger(categoryName, _levels);
+        }
+
+        public void AddProvider(ILoggerProvider provider)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class UnityMcpLogHandler : ILogHandler
+    {
+        private static ILogHandler _defaultLogger;
+
+        public UnityMcpLogHandler()
+        {
+            _defaultLogger = Debug.unityLogger.logHandler;
+        }
+
+        public void LogFormat(LogType logType, Object context, string format, params object[] args)
+        {
+            if (!format.StartsWith("{"))
+            {
+                return;
+            }
+
+            _defaultLogger.LogFormat(logType, context, format, args);
+            ;
+        }
+
+        public void LogException(Exception exception, Object context)
+        {
+            return;
+        }
+    }
+}
