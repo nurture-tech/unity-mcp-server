f0c4f784bf76a38aab98567fc0ee76bdc5c0e3ac
Alex Stone <stone@filamentgames.com>
2025-06-25 00:44:28 -0500

    wip pass arguments to unity

--- packages/node-runner/src/index.ts
+++ packages/node-runner/src/index.ts
@@ -1,30 +1,14 @@
-﻿#!/usr/bin/env node
-
-import { readFileSync } from "node:fs";
-import { execSync, exec } from "node:child_process";
-import { platform, exit } from "node:process";
+﻿import { spawn } from "node:child_process";
+import { exit } from "node:process";
 import { ArgumentParser } from "argparse";
 
 const parser = new ArgumentParser();
-parser.add_argument("-projectPath", { type: String, required: true });
+parser.add_argument("-unityPath", { type: String, required: true });
+parser.add_argument("", { nargs: "*" });
 const args = parser.parse_args();
-const projectPath = args.projectPath;
-const projectVersion = readFileSync(`${projectPath}/ProjectSettings/ProjectVersion.txt`, "utf8").match(/m_EditorVersion:\s*(.*)/)?.[1] || "";
+const unityPath = args.unityPath;
 
-const editorPathCommand =
-  platform === "darwin"
-    ? `/Applications/Unity\\ Hub.app/Contents/MacOS/Unity\\ Hub -- --headless editors -i ${projectVersion}`
-    : `"${process.env.PROGRAMFILES}\\Unity Hub\\Unity Hub.exe" -- --headless editors -i  ${projectVersion}`;
-
-const editorPaths = String(execSync(editorPathCommand)).trim();
-
-const editorPath =
-  `${editorPaths}`
-    .split("\n")
-    .find((line) => line.includes(projectVersion))
-    ?.split(" , installed at ")[1] || "";
-
-const proc = exec(`${editorPath} ${process.argv.slice(1).join(" ")} -mcp -logFile -`);
+const proc = spawn(unityPath, [...process.argv.slice(1), "-mcp", "-logFile", "-"]);
 
 const code = await new Promise<number | null>((resolve) => {
   process.stdin.pipe(proc.stdin!);
